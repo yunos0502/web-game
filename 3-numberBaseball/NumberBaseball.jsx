83667a51a816854a1b46d6420bd84248bc263acc
Park Gyu Ae <yunos0502@gmail.com>
2020-11-25 02:08:15 +0900

    1125 web-game add prevState

--- 3-numberBaseball/NumberBaseball.jsx
+++ 3-numberBaseball/NumberBaseball.jsx
@@ -26,9 +26,11 @@ class NumberBaseball extends Component {
     e.preventDefault();
 
     if (value === answer.join('')) {
-      this.setState({
-        result: '홈런!',
-        tries: [...tries, { try: value, result: '홈런!' }],
+      this.setState((prevState) => {
+        return {
+          result: '홈런!',
+          tries: [...prevState.tries, { try: value, result: '홈런!' }],
+        };
       });
 
       alert('게임을 다시 시작합니다!');
@@ -63,14 +65,16 @@ class NumberBaseball extends Component {
             ball += 1;
           }
         }
-        this.setState({
-          tries: [
-            ...tries,
-            {
-              try: value,
-              result: `${strike} 스트라이크, ${ball} 볼입니다!`,
-            },
-          ],
+        this.setState((prevState) => {
+          return {
+            tries: [
+              ...prevState.tries,
+              {
+                try: value,
+                result: `${strike} 스트라이크, ${ball} 볼입니다!`,
+              },
+            ],
+          };
         });
       }
     }
