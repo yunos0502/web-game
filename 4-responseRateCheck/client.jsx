e62bf93b890aeeb7b9f37d747a4a6b163bd7e46b
Park Gyu Ae <yunos0502@gmail.com>
2020-12-04 01:51:55 +0900

    1204 web-game ResponseCheck class -> hook changed

--- 4-responseRateCheck/client.jsx
+++ 4-responseRateCheck/client.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ResponseCheck from './ResponseCheck';
+// import ResponseCheckClass from './ResponseCheckClass';
 
 ReactDOM.render(<ResponseCheck />, document.querySelector('#root'));
