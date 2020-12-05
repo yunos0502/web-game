9fd743a3f7470878c24dc99eb2ecfd0575bd994e
Park Gyu Ae <yunos0502@gmail.com>
2020-12-05 15:22:58 +0900

    1205 web-game 묵찌빠 setting

---
+++ 5-mukchipa/RSP.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class RSP extends Component {
+  render() {
+    return <div>묵찌빠</div>;
+  }
+}
---
+++ 5-mukchipa/client.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RSP from './RSP';
+
+ReactDOM.render(<RSP />, document.querySelector('#root'));
