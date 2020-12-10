289e9d4336f01974fac711f51dd4aaddee22b574
Park Gyu Ae <yunos0502@gmail.com>
2020-12-11 02:09:04 +0900

    1211 web-game Lotto ref

--- 6-lotto/Lotto.jsx
+++ 6-lotto/Lotto.jsx
@@ -51,6 +51,19 @@ const Lotto = () => {
   }, [timeouts.current]); // 빈 배열이면 componentDidMount와 동일
   // 배열에 요소가 있으면 componentDidMount와 componentDidUpdate 둘 다 수행
 
+  // useEffect(() => {
+  //   // ajax
+  // }, []); // componentDidUpdate X / componentDidMount O
+
+  // const mounted = useRef(false);
+  // useEffect(() => {
+  //   if (!mounted.current) {
+  //     mounted.current = true;
+  //   } else {
+  //     // ajax
+  //   }
+  // }, [바뀌는값]); // componentDidUpdate O / componentDidMount X
+
   const onClickRedo = useCallback(() => {
     console.log('ClickRedo');
     console.log(winNumbers);
