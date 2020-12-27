2a8074c15f91ed3406953991dfb3956f3691c907
Park Gyu Ae <yunos0502@gmail.com>
2020-12-27 17:24:16 +0900

    1227 web-game 동적 라우트 매칭 & queryString & URLSearchParams

--- react-router/GameMatcher.jsx
+++ react-router/GameMatcher.jsx
@@ -5,14 +5,19 @@ import Lotto from '../6-lotto/Lotto_class';
 
 class GameMatcher extends Component {
   render() {
-    console.log(this.props);
-    return (
-      <>
-        <NumberBaseball />
-        <RSP />
-        <Lotto />
-      </>
+    let urlSearchParams = new URLSearchParams(
+      this.props.location.search.slice(1)
     );
+    console.log(urlSearchParams.get('name'));
+    if (this.props.match.params.name === 'number-baseball') {
+      return <NumberBaseball />;
+    } else if (this.props.match.params.name === 'rock-scissors-paper') {
+      return <RSP />;
+    } else if (this.props.match.params.name === 'lotto-generator') {
+      return <Lotto />;
+    }
+
+    return <div>일치하는 게임이 없습니다.</div>;
   }
 }
 
--- react-router/Games.jsx
+++ react-router/Games.jsx
@@ -5,9 +5,9 @@ import GameMatcher from './GameMatcher';
 const Games = () => {
   return (
     <BrowserRouter>
-      <Link to='/game/number-baseball'>숫자야구</Link>&nbsp;
-      <Link to='/game/rock-scissors-paper'>가위바위보</Link>&nbsp;
-      <Link to='/game/lotto-generator'>로또생성기</Link>&nbsp;
+      <Link to='/game/number-baseball?id=1&name=baseball'>숫자야구</Link>&nbsp;
+      <Link to='/game/rock-scissors-paper?id=2&name=rsp'>가위바위보</Link>&nbsp;
+      <Link to='/game/lotto-generator?id=3&name=lotto'>로또생성기</Link>&nbsp;
       <Link to='/game/index'>게임 매쳐</Link>
       <div>
         <Route path='/game/:name' component={GameMatcher} />
